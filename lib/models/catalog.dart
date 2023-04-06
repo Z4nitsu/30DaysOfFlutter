e7faea2771c20fa443f18213e87cc0414e613f49
Pranav <pranavchamoli08@gmail.com>
2023-04-06 19:40:22 +0530

    day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 14 pro",
+        desc: "Apple iphone 14th generation",
+        price: 999,
+        color: "33505a",
+        image:
+            "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-unselect-gallery-2-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1663790290203")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -14,14 +27,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-      id: "Pranav",
-      name: "iphone 14 pro",
-      desc: "Apple iphone 14th generation",
-      price: 999,
-      color: "33505a",
-      image:
-          "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-unselect-gallery-2-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1663790290203")
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 import '../widgets/drawer.dart';
-//today we learned about contexts,constraints
+
 class HomePage extends StatelessWidget {
   final int days = 30;
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(15, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
         titleTextStyle: TextStyle(color: Colors.black),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of Flutter"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
